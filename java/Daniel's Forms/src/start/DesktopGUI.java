3f2e5492b03da7cd299480b3a38d021aa9b3ecba
Josh Gillham <usajoshgillham@gmail.com>
2013-11-28 11:27:03 -0700

    Refactored DesktopGUI to work with Application.

--- java/Daniel's Forms/src/start/DesktopGUI.java
+++ java/Daniel's Forms/src/start/DesktopGUI.java
@@ -4,40 +4,47 @@
  */
 package start;
 
+import control.Application;
+import java.sql.SQLException;
+import java.sql.SQLNonTransientConnectionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import view.login;
 
 /**
  *
  * @author ArcadeHitman
  */
-public class JavaApplication1 {
+public class DesktopGUI {
 
     private String userName;
-    final String DB_URL = "jdbc:derby://localhost:1527/testing";//for local testing
+    static final String DB_URL = "jdbc:derby://localhost:1527/testing";//for local testing
     //final String DB_URL = "jdbc:oracle:thin:@//cncsidb01.msudenver.edu:1521:DB01, S900723387, kYmyal5v";
     boolean login;
 
     /**
+     * Boot straps a Desktop GUI application.
+     * 
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try{
-            Class.forName("org.apache.derby.jdbc.ClientDriver");
+        if ( !Application.testDriver() ) {
+            System.exit( 1 );
         }
-        // If an exception is generated then try removing the driver,
-        //  closing the project, re-opening the project, and finally
-        //  re-adding the driver.
-        catch (Exception e) {
-            e.printStackTrace();
+        Application app;
+        try {
+            app = Application.getApplication( DB_URL );
         }
-        JavaApplication1 app = new JavaApplication1();
+        // Can occur when the server is not running or when the database name
+        //  is not found.
+        catch (SQLException ex) {
+            Logger.getLogger(DesktopGUI.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+            System.exit( 3 );
+        }
         login form = new login();
         form.setVisible(true);
-        form.DB_URL = app.DB_URL;
-        System.out.println("the db url is " + form.DB_URL);
-        app.userName = form.returnUName();
-        app.login = form.loginCheck();
-
-        }
+        System.out.println("the db url is " + DB_URL);
+    }
     
 }
